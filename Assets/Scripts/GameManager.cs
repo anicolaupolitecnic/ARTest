51be3cdc416968b4c598908394df76e8922c33bb
anicolaupolitecnic <anicolau@politecnicllevant.cat>
2024-01-31 13:47:08 +0100

    canvi de tracking d'imatges a planeDetector

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,12 +12,18 @@ public class GameManager : MonoBehaviour
     public Camera aRCamera;
 
     //GENERAL
+    [SerializeField] private TextMeshProUGUI debugText;
     [SerializeField] private GameObject mainMenu;    
     [SerializeField] private GameObject modeAdventure;
     [SerializeField] private GameObject modeCircuit;
 
+    private const int modeMainMenu = 0;
+    private const int modeAdv = 1;
+    private const int modeCir = 2;
+    private int mode = 0;
+
     //Mode Adventure
-    private bool isPlacableCar;
+    private bool isPlacableCar = false;
     [SerializeField] private ARMeshManager arMeshManager;
     [SerializeField] private GameObject meshing;
     [SerializeField] private TextMeshProUGUI scanButtonText;
@@ -26,7 +32,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject car;
 
     //Mode Circuit
-    [SerializeField] private ARTrackedImageManager tracking;
+    private bool isPlacableCircuit = false;
+    [SerializeField] private ARPlaneManager planeManager;
+    //[SerializeField] private ARTrackedImageManager tracking;
 
     void Awake()
     {
@@ -39,14 +47,21 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
-        EnableModeCircuit();
+       // EnableModeCircuit();
     }
 
     void Update()
     {
-        if (isPlacableCar)
+        if (mode == modeAdv)
         {
-            SetCarPosition();
+            if (isPlacableCar)
+                SetCarPosition();
+        } else if (mode == modeCir)
+        {
+            if (isPlacableCircuit)
+            {
+                SetCircuitPosition();
+            }
         }
     }
 
@@ -63,7 +78,7 @@ public class GameManager : MonoBehaviour
         mainMenu.SetActive(false);
         modeAdventure.SetActive(false);
         modeCircuit.SetActive(true);
-        tracking.enabled = true;
+        planeManager.enabled = true;
     }
 
     public void BackButton()
@@ -71,7 +86,7 @@ public class GameManager : MonoBehaviour
         modeAdventure.SetActive(false);
         meshing.SetActive(false);
         modeCircuit.SetActive(false);
-        tracking.enabled = false;
+        planeManager.enabled = false;
         mainMenu.SetActive(true);
     }
 
@@ -108,6 +123,38 @@ public class GameManager : MonoBehaviour
                     isPlacableCar = false;
                     if (!carHUD.activeSelf)
                         carHUD.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void SetCircuitPosition()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                // Get the position of the touch
+                Vector2 touchPosition = Input.GetTouch(0).position;
+
+                // Convert the touch position to a ray from the camera
+                Ray ray = aRCamera.ScreenPointToRay(touchPosition);
+
+                // Perform the raycast
+                RaycastHit hit;
+
+                //Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 100f);
+                if (Physics.Raycast(ray, out hit))
+                {
+
+                    if (hit.collider.gameObject.name == "")
+                    {
+                        //Spawn circuit
+
+                    }
+                    debugText.text = "" + hit.collider.gameObject.name;
+                    isPlacableCircuit = false;
+                    
                 }
             }
         }
