381d81ba0ae1b22a062dc66eb00fd972cf37e55b
xamorro <martibp2000@gmail.com>
2024-02-02 21:00:14 +0100

    contador reset spawn car

--- Assets/Scripts/Marti/Contador.cs
+++ Assets/Scripts/Marti/Contador.cs
@@ -6,14 +6,32 @@ using UnityEngine;
 public class Contador : MonoBehaviour
 {
     public float TimerAdventure = 0;
+
+
+    [SerializeField] private GameObject Temps;
     [SerializeField] private TextMeshProUGUI ContadorTexte;
+    [SerializeField] private GameObject car;
     // Start is called before the first frame update
 
     void Update()
     {
 
-        TimerAdventure += Time.deltaTime;
+        if (car.activeInHierarchy)
+        {
+            Temps.SetActive(true);
+            TimerAdventure += Time.deltaTime;
 
-        ContadorTexte.text = "" + TimerAdventure.ToString("f1");
+            ContadorTexte.text = "" + TimerAdventure.ToString("f1");
+        }
+        else
+        {
+            Temps.SetActive(false);
+            TimerAdventure = 0;
+
+        }
+
+
+
+
     }
 }
