540d35bf0de1eb0e0990c7a22b5b309ca656ae92
anicolauiesmanacor <anicolau@politecnicllevant.cat>
2024-02-09 20:53:41 +0100

    Treballant en la mecànica i hud del mode adventure

--- Assets/Scripts/ModeAdvManager.cs
+++ Assets/Scripts/ModeAdvManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,6 +7,10 @@ using UnityEngine.XR.ARFoundation;
 
 public class ModeAdvManager : MonoBehaviour
 {
+    private float timer;
+    private float playTime;
+    private float targetTime = 3;
+    private bool scanTimeDone = false;
     private GameManager gameManager;
     public Camera aRCamera;
 
@@ -15,9 +20,19 @@ public class ModeAdvManager : MonoBehaviour
     [SerializeField] private ARMeshManager arMeshManager;
     [SerializeField] private GameObject meshing;
     [SerializeField] private TextMeshProUGUI scanButtonText;
-    [SerializeField] private GameObject spawnCarButton;
+   
+    [SerializeField] private GameObject respawnCarButton;
+    [SerializeField] private TextMeshProUGUI respawnCarText;
+    private int respawnCounter;
+
     [SerializeField] private GameObject carHUD;
     [SerializeField] private GameObject car;
+
+    [SerializeField] private TextMeshProUGUI messageText;
+    
+    [SerializeField] private TextMeshProUGUI timerTxt;
+    int minutes;
+    int seconds;
 
 
     private void Awake()
@@ -28,20 +43,44 @@ public class ModeAdvManager : MonoBehaviour
     void Start()
     {
         isPlacableCar = false;
+        messageText.text = "Escaneja el terra!";
 
     }
 
     void Update()
     {
-        if (gameManager.mode == gameManager.MODEADV)
+        timer += Time.deltaTime;
+
+        if (timer >= targetTime && !scanTimeDone)
         {
-            Debug.Log("Entra modo adv");
+            scanTimeDone = true;
+            if (gameManager.mode == gameManager.MODEADV)
+            {
+                messageText.text = "Toca al terra on vols que aparegui el cotxe!";
+                isPlacableCar = true;
+            }
+        } 
+        else
+        {
+            playTime = Time.time - timer;
+            minutes = Mathf.FloorToInt(playTime / 60f);
+            seconds = Mathf.FloorToInt(playTime % 60f);
+            timerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
             if (isPlacableCar)
             {
-                Debug.Log("Entra isPlacable");
                 SetCarPosition();
             }
-                
+        }
+    }
+
+    private void UpdateSecurityNet()
+    {
+        GameObject cube = GameObject.Find("Cube");
+
+        if (cube != null)
+        {
+            cube.transform.position = new Vector3(car.transform.position.x, car.transform.position.y - 0.04f, car.transform.position.z);
         }
     }
 
@@ -57,34 +96,33 @@ public class ModeAdvManager : MonoBehaviour
     {
         isPlacableCar = true;
         car.SetActive(false);
-
-        Invoke("AddSecurityNet", 1f);
+        //spawnCarButton.SetActive(false);
     }
 
-    void AddSecurityNet()
-    {
-        GameObject cubo = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cubo.transform.localScale = new Vector3(100f, 100f, 1f);
-        cubo.transform.position = new Vector3(car.transform.position.x, car.transform.position.y, car.transform.position.z - 1.785f);
+    //void AddSecurityNet()
+    //{
+    //    GameObject cubo = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //    cubo.transform.localScale = new Vector3(1f, 1f, 0.05f);
+    //    cubo.transform.position = new Vector3(car.transform.position.x, car.transform.position.y - 0.04f, car.transform.position.z);
 
-        cubo.transform.Rotate(Vector3.left, 90f);
+    //    cubo.transform.Rotate(Vector3.left, 90f);
 
-        cubo.GetComponent<Renderer>().enabled = false;
+    //    cubo.GetComponent<Renderer>().enabled = false;
 
-        Rigidbody rb = cubo.AddComponent<Rigidbody>();
-        rb.isKinematic = true;
+    //    Rigidbody rb = cubo.AddComponent<Rigidbody>();
+    //    rb.isKinematic = true;
+    //    rb.useGravity = false;
+    //    rb.constraints = RigidbodyConstraints.FreezeRotation;
 
-        BoxCollider boxCollider = cubo.AddComponent<BoxCollider>();
-    }
+    //    BoxCollider boxCollider = cubo.AddComponent<BoxCollider>();
+    //}
 
     private void SetCarPosition()
     {
         if (Input.touchCount > 0)
         {
-            Debug.Log("Entra touck 1");
             if (Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                Debug.Log("Entra touck 2");
                 // Get the position of the touch
                 Vector2 touchPosition = Input.GetTouch(0).position;
 
@@ -97,7 +135,6 @@ public class ModeAdvManager : MonoBehaviour
                 //Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 100f);
                 if (Physics.Raycast(ray, out hit))
                 {
-                    Debug.Log("Entra touck 3");
                     car.SetActive(true);
                     Vector3 t = hit.point;
                     t = new Vector3(t.x, t.y + 0.5f, t.z);
@@ -107,9 +144,37 @@ public class ModeAdvManager : MonoBehaviour
                     isPlacableCar = false;
                     if (!carHUD.activeSelf)
                         carHUD.SetActive(true);
+
+                    //Invoke("AddSecurityNet", 1f);
+                    messageText.text = "";
+                    respawnCarButton.SetActive(true);
+                    respawnCarText.enabled = true;
+                    timerTxt.enabled = true;
                 }
             }
         }
+    }
+
+    public void RespawnCar()
+    {
+        isPlacableCar = true;
+
+        Vector3 t = car.transform.position;
+        t = new Vector3(t.x, t.y + 0.5f, t.z);
+        car.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        car.transform.position = t;
+        ResetCarValues();
+        isPlacableCar = false;
+        if (!carHUD.activeSelf)
+            carHUD.SetActive(true);
+
+        //Invoke("AddSecurityNet", 1f);
+        messageText.text = "";
+        if (!respawnCarButton.activeSelf)
+            respawnCarButton.SetActive(true);
+
+        respawnCounter++;
+        respawnCarText.text = respawnCounter + " Respawns";
     }
 
     private void ResetCarValues()
